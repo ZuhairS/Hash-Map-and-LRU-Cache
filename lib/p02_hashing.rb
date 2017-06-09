72a2114cdd80ef6183bdb52b681dc44bf654c74e
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-06-09 14:30:11 -0700

    Create working hash_set

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    hashstr = ""
+    each do |ele|
+      hashstr += ele.object_id.to_s(2)
+    end
+    hashstr.to_i.hash
   end
 end
 
 class String
   def hash
+    chararr = chars
+    hashstr = ""
+    chararr.each do |lttr|
+      hashstr += lttr.ord.to_s(2)
+    end
+    hashstr.to_i.hash
   end
 end
 
@@ -16,6 +27,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hashsum = 0
+    each do |k, v|
+      hashsum += k.to_s.hash + v.to_s.hash
+    end
+    hashsum.hash
   end
 end
--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,18 +9,33 @@ class HashSet
   end
 
   def insert(key)
+    inspect
+    unless include?(key)
+      self[key.hash] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
+  end
+
+  def show
+    @store
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +43,15 @@ class HashSet
   end
 
   def resize!
+    allnum = @store.flatten
+    # test_case = ResizingIntSet.new(num_buckets * 2)
+    initialize(num_buckets * 2)
+    until allnum.empty?
+      self.insert(allnum.pop)
+    end
+  end
+
+  def inspect
+    resize! if @count >= num_buckets
   end
 end
